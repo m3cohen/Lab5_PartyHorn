791d27a29cf841c2b24936c1c44702f4fd000277
m3cohen <60684461+m3cohen@users.noreply.github.com>
2021-02-02 15:30:52 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,10 @@
 // main.js
 
-// TODO
+const input = document.querySelector('input');
+const log = document.getElementById('volume-number');
+
+input.addEventListener('input', updateValue);
+
+function updateValue(e) {
+  log.textContent = e.target.value;
+}
